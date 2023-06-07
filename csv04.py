bd365cacb2aef9ab88848e7642252d6ccdf99af8
JaredKipngetich <119751579+JaredKipngetich@users.noreply.github.com>
2023-06-07 21:00:34 +0300

    Add files via upload

---
+++ csv04.py
@@ -0,0 +1,17 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import csv
+file=open("Stars.csv","r")
+for row in file:
+    print(row)
+
+    
+Brian,73,Taurus
+
+file=open("stars.csv","r")
+for row in file:
+    print(row)
+
+    
+Brian,73,Taurus
+
